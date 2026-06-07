f6839e3862a6c5ba928bf54a9f5f530a054c6d9f
Sakshilal2005 <sakshilal2005@gmail.com>
2026-06-07 21:48:11 +0530

    Fix OTP email nodemailer config

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,8 +3,12 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
