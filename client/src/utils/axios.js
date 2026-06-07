bf4fd69db7c3b2a35bcab2ff9bedce75f1c5d5d1
Sakshilal2005 <sakshilal2005@gmail.com>
2026-06-07 19:07:52 +0530

    fix register form bug

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://event-mern-ha4f.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
