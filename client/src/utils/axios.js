f5349dfc6e33bbc0d63285a327b753d51aaee80d
Sakshilal2005 <sakshilal2005@gmail.com>
2026-06-07 17:16:44 +0530

    Update API URL

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://event-mern-ha4f.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
-    }, 
+    },
 });
 
 api.interceptors.request.use((config) => {
